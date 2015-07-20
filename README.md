02c333c505281272f4c7a544fec3b5e397438496
Liam <lnguy058@ucr.edu>
2015-07-20 10:58:04 -0700

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# ElevenAM.github.io
+In this repo, you will find the most up-to-date version of my online portfolio
+
+To use
+------
+Visit elevenam.github.io
+
+Projects are listed and can be clicked on for further information/ links to their respective github repos/ pages.
+
+You may contact me at liamnguyen.mail@gmail.com
---
+++ js/app.js
@@ -0,0 +1,90 @@
+function appViewModel() {
+    function wrapInRows() {
+        var allRows = [];
+        var row;
+        self.projects.forEach(function (project, i) {
+            if (i % self.cols === 0) {
+                if (row) {
+                    allRows.push(row);
+                }
+                row = {};
+                row.content = [];
+            }
+            row.content.push(project);
+        });
+
+        if (row) {
+            allRows.push(row);
+        }
+
+        return allRows;
+    }
+
+    self.cols = 2;
+
+    self.colName = (function () {
+        return 'col-md-' + 12 / self.cols;
+    })();
+
+    self.projects = [
+        {
+            name: 'Arcade Game',
+            url: 'http://elevenam.github.io/Hive',
+            github: 'http://github.com/elevenam/Hive',
+            image: 'images/Hive.png',
+            description: "Utilized JavaScript inside HTML5 Canvas to recreate Frogger with a gun/ ammo system on top. Exercised mastery of loops, conditionals, objects and arrays",
+            date: 'April 2015'
+        },
+        {
+            name: 'Neighborhood Map',
+            url: 'http://elevenam.github.io/Neighborhood-Map',
+            github: 'http://github.com/elevenam/Neighborhood-Map',
+            image: 'images/neighborhood.png',
+            description: "Used the Google Places/ Maps API to populate an interactive Neighborhood Map of Huntington Beach, CA. "
+            + "A list on the side interacts with markers on the map to display restaurant information.  " +
+            "The Instagram API was used to allow users to browse pictures of each establishment."  +
+            "Exercised mastery of API’s, Dynamic UI’s, Asynchronous requests and error handling",
+            skills: ['coniunctione', 'cillum', 'se', 'nisi'],
+            date: 'June 2015'
+        },
+        {
+            name: 'Jasmine Test Suite',
+            url: 'http://elevenam.github.io/Feedreader-Testing',
+            github: 'http://github.com/elevenam/Feedreader-Testing',
+            image: 'images/Jasmine-Testing.png',
+            description: "Used the Google Places/ Maps API to populate an interactive Neighborhood Map of Huntington Beach, CA.  " +
+            "A list on the side interacts with markers on the map to display restaurant information. " +
+            " The Instagram API was used to allow users to browse pictures of each establishment. " +
+            "Exercised mastery of API’s, Dynamic UI’s, Asynchronous requests and error handling",
+            skills: ['coniunctione', 'cillum', 'se', 'nisi'],
+            date: 'July 2014'
+        },
+        {
+            name: 'Website Optimization',
+            url: 'http://elevenam.github.io/Website-Optimization/views/pizza.html',
+            github: 'http://github.com/elevenam/Website-Optimization',
+            image: 'images/website_optimization.png',
+            description: "Used the Google Places/ Maps API to populate an interactive Neighborhood Map of Huntington Beach, CA. " +
+            "A list on the side interacts with markers on the map to display restaurant information. " +
+            "The Instagram API was used to allow users to browse pictures of each establishment. "+
+            "Exercised mastery of API’s, Dynamic UI’s, Asynchronous requests and error handling",
+            skills: ['coniunctione', 'cillum', 'se', 'nisi'],
+            date: 'May 2015'
+        }
+    ];
+
+    self.projects.forEach(function (project, i) {
+        project.id = 'project' + i;
+        project.idWithHash = '#' + project.id;
+    });
+
+    self.projectsInRows = wrapInRows();
+
+    self.chosenProject = ko.observable();
+
+    self.goToProject = function (project) {
+        self.chosenProject(project);
+    };
+}
+
+ko.applyBindings(new appViewModel());
