8c6457d40bacde505be57cf360f7de8a4e7e1bd2
Liam <lnguy058@ucr.edu>
2015-07-20 14:14:06 -0700

    Updated project descriptions and added some mobile CSS that will make the images display better at smaller screensizes

--- js/app.js
+++ js/app.js
@@ -44,7 +44,6 @@ function appViewModel() {
             + "A list on the side interacts with markers on the map to display restaurant information.  " +
             "The Instagram API was used to allow users to browse pictures of each establishment."  +
             "Exercised mastery of API’s, Dynamic UI’s, Asynchronous requests and error handling",
-            skills: ['coniunctione', 'cillum', 'se', 'nisi'],
             date: 'June 2015'
         },
         {
@@ -52,11 +51,11 @@ function appViewModel() {
             url: 'http://elevenam.github.io/Feedreader-Testing',
             github: 'http://github.com/elevenam/Feedreader-Testing',
             image: 'images/Jasmine-Testing.PNG',
-            description: "Used the Google Places/ Maps API to populate an interactive Neighborhood Map of Huntington Beach, CA.  " +
-            "A list on the side interacts with markers on the map to display restaurant information. " +
-            " The Instagram API was used to allow users to browse pictures of each establishment. " +
-            "Exercised mastery of API’s, Dynamic UI’s, Asynchronous requests and error handling",
-            skills: ['coniunctione', 'cillum', 'se', 'nisi'],
+            description: "Test-driven development is a great way to approach development. " +
+            " Instead of just rushing in to code, we build a series of tests that stress our desired functionality. " +
+            "As we build, tests will go from pass to fail and when all the tests pass, we should have a working version of our desired product." +
+            "In this exercise, I was given an existing Feedreader application. " +
+             "My job was to use the Jasmine library to design tests that would make sure that each feature was accounted for and worked correctly",
             date: 'July 2014'
         },
         {
@@ -64,11 +63,9 @@ function appViewModel() {
             url: 'http://elevenam.github.io/Website-Optimization/views/pizza.html',
             github: 'http://github.com/elevenam/Website-Optimization',
             image: 'images/website_optimization.png',
-            description: "Used the Google Places/ Maps API to populate an interactive Neighborhood Map of Huntington Beach, CA. " +
-            "A list on the side interacts with markers on the map to display restaurant information. " +
-            "The Instagram API was used to allow users to browse pictures of each establishment. "+
-            "Exercised mastery of API’s, Dynamic UI’s, Asynchronous requests and error handling",
-            skills: ['coniunctione', 'cillum', 'se', 'nisi'],
+            description: "Given a page with a Google Page Score of 35/100, I reorganized the HTML, " +
+            "inlined critical CSS streamlined JavaScript functions, minified files and compressed " +
+            "images to finish with a Google Page Score of 97/100",
             date: 'May 2015'
         }
     ];
