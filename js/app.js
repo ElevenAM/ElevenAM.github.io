f82bc4f571155d950f796f24d9d75a6372cb0d6c
Liam <liamnguyen.mail@gmail.com>
2015-10-30 12:24:52 -0700

    Updated with latest projects from Arbonne. Changed contact information. Adjusted "About Me" to include testimonials

--- js/app.js
+++ js/app.js
@@ -28,12 +28,20 @@ function appViewModel() {
 
     self.projects = [
         {
-            name: 'Arcade Game',
-            url: 'http://elevenam.github.io/Hive',
-            github: 'http://github.com/elevenam/Hive',
-            image: 'images/Hive.PNG',
-            description: "This project demonstrates my ability to imagine and create fun and engaging games. In this example I utilized JavaScript inside HTML5 Canvas to recreate Frogger with a few new extras. It features ammo/gun system, five different enemies, lives, and progressive difficulty! Exercised mastery of loops, conditionals, objects, inheritance, and arrays. Check out what happens at level 15!",
-            date: 'April 2015'
+            name: 'Arbonne Promotions Page',
+            url: 'http://promotions.arbonne.com',
+            github: '',
+            image: 'images/promotionsite.png',
+            description: "This project was one of the solo-endeavors I am most proud of. The site is fully responsive on all screens and includes a drop-down menu which changes content based on the selection. The two more tricky features, however, are the product description buttons that float over the product images and the \"Share\" button. The product description buttons are generated dynamically via JSON object for both large/ small screen options and locked to percentage coordinates on the image. The \"Share\" button can detect which product set you are looking at and when you share the page via social media, the link will automatically open the correct product set and scroll the viewer down to the set.",
+            date: 'October 2015'
+        },
+        {
+            name: 'Arbonne Event Microsite',
+            url: 'http://arbonne-events.arbonne.com/nvp/',
+            github: '',
+            image: 'images/microsite.png',
+            description: "Given a photoshop image, I created a pixel perfect representation of the desired event microsite. Notable extra elements include a Navigation bar that tracks where the user is on the page, a \"Print\" button that automatically adjusts content to be print friendly and the usage of modals/ carousels.",
+            date: 'August 2015'
         },
         {
             name: 'Neighborhood Map',
@@ -44,20 +52,28 @@ function appViewModel() {
             date: 'June 2015'
         },
         {
-            name: 'Jasmine Test Suite',
-            url: 'http://elevenam.github.io/Feedreader-Testing',
-            github: 'http://github.com/elevenam/Feedreader-Testing',
-            image: 'images/Jasmine-Testing.PNG',
-            description: "This project represents my ability to adapt to a specific development procedure and produce viable code. Test-driven development is a great way to approach development. Instead of just rushing into code, it's important to build a series of tests that stress the desired functionality. As tests are built, results vary from pass to fail, and when all the tests pass, a working version of the desired product has been accomplished. In this exercise, I was given an existing Feedreader application. My job was to use the Jasmine library to design tests that would make sure each feature was accounted for and worked correctly.",
-            date: 'July 2014'
-        },
-        {
             name: 'Website Optimization',
             url: 'http://elevenam.github.io/Website-Optimization/views/pizza.html',
             github: 'http://github.com/elevenam/Website-Optimization',
             image: 'images/website_optimization.png',
             description: "This project demonstrates my ability to improve the code efficiency of a Web page. Initially I was given a page with a Google Page Score of 35/100. I was able to reorganize the HTML, inlined critical CSS, streamlined the JavaScript functions, minimized files, and compressed images to finish with a Google Page Score of 97/100.",
             date: 'May 2015'
+        },
+        {
+            name: 'Arcade Game',
+            url: 'http://elevenam.github.io/Hive',
+            github: 'http://github.com/elevenam/Hive',
+            image: 'images/Hive.PNG',
+            description: "This project demonstrates my ability to imagine and create fun and engaging games. In this example I utilized JavaScript inside HTML5 Canvas to recreate Frogger with a few new extras. It features ammo/gun system, five different enemies, lives, and progressive difficulty! Exercised mastery of loops, conditionals, objects, inheritance, and arrays. Check out what happens at level 15!",
+            date: 'April 2015'
+        },
+        {
+            name: 'Jasmine Test Suite',
+            url: 'http://elevenam.github.io/Feedreader-Testing',
+            github: 'http://github.com/elevenam/Feedreader-Testing',
+            image: 'images/Jasmine-Testing.PNG',
+            description: "This project represents my ability to adapt to a specific development procedure and produce viable code. Test-driven development is a great way to approach development. Instead of just rushing into code, it's important to build a series of tests that stress the desired functionality. As tests are built, results vary from pass to fail, and when all the tests pass, a working version of the desired product has been accomplished. In this exercise, I was given an existing Feedreader application. My job was to use the Jasmine library to design tests that would make sure each feature was accounted for and worked correctly.",
+            date: 'July 2014'
         }
     ];
 
